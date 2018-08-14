115220436d3a13b0b792ca23ea79bb008376d3f0
Toucher-F <lianduanb@gmail.com>
2018-08-14 18:02:11 -0400

    worker thread

--- index.js
+++ index.js
@@ -3,11 +3,24 @@
 const express = require("express");
 const app = express();
 const crypto = require("crypto");
+const Worker = require("webworker-threads").Worker;
 
 app.get("/", (req, res) => {
-  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-    res.send("Hi there");
+  const worker = new Worker(function() {
+    this.onmessage = function() {
+      let counter = 0;
+      while (counter < 1e9) {
+        counter++;
+      }
+      postMessage(counter);
+    };
   });
+  worker.onmessage = function(message) {
+    console.log(message.data);
+    res.send("" + message.data);
+  };
+
+  worker.postMessage();
 });
 
 app.get("/fast", (req, res) => {
